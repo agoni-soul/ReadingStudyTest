2ba409a9ffa5895f3e81bb248c88d35783b56a26
agoni-soul <57203442+agoni-soul@users.noreply.github.com>
2019-11-07 21:26:14 +0800

    新建home界面的androidfragment的网络请求
    已经搭建好了请求框架，还需要完成如何从网络请求数据写入到本地，并进行保存

---
+++ app/src/main/java/com/readingstudytest/home/android_item_content.java
@@ -0,0 +1,85 @@
+package com.readingstudytest.home;
+
+public class android_item_content {
+    private String apkLink;
+    private int audit;
+    private String author;
+    private int chapterId;
+    private String chapterName;
+    private boolean collect;
+    private int courseId;
+    private String desc;
+    private String envelopPic;
+    private boolean fresh;
+    private int id;
+    private String link;
+    private String niceDate;
+    private String niceShareDate;
+    private String origin;
+    private String prefix;
+    private String projectLink;
+    private long publicTime;
+    private int selfVisible;
+    private long shareDate;
+    private String shareUser;
+    private int superChapterId;
+    private String superChapterName;
+    private String[] tags;
+    private String title;
+    private int type;
+    private int userId;
+    private int visible;
+    private int zan;
+
+    public String getApkLink(){
+        return apkLink;
+    }
+
+    public void setApkLink(String apkLink){
+        this.apkLink = apkLink;
+    }
+
+    public int getAudit(){
+        return audit;
+    }
+
+    public void setAudit(int audit){
+        this.audit = audit;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public int getChapterId(){
+        return chapterId;
+    }
+
+    public void setChapterId(int chapterId){
+        this.chapterId = chapterId;
+    }
+
+    public String getChapterName(){
+        return chapterName;
+    }
+
+    public void setChapterName(String chapterName){
+        this.chapterName = chapterName;
+    }
+
+    public boolean getCollect(){
+        return collect;
+    }
+
+    public void setCollect(boolean collect){
+        this.collect = collect;
+    }
+
+    public int getCourseId(){
+        return courseId;
+    }
+}
