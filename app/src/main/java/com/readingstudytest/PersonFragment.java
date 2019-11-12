101d32c97e7e3160d8b364e3ea9a952ab9671e02
agoni-soul <57203442+agoni-soul@users.noreply.github.com>
2019-11-12 14:13:21 +0800

    Appbarlayout中addOnOffsetChangedListener()方法监听失败
    这部分出现一个bug

--- app/src/main/java/com/readingstudytest/PersonFragment.java
+++ app/src/main/java/com/readingstudytest/PersonFragment.java
@@ -1,20 +1,41 @@
 package com.readingstudytest;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.readingstudytest.login.LoginActivity;
 
-public class PersonFragment extends Fragment {
+import butterknife.BindView;
+
+public class PersonFragment extends Fragment implements View.OnClickListener, AppBarLayout.OnOffsetChangedListener{
+
+    @BindView(R.id.mine_appbar_layout)
+    AppBarLayout mineAppbarLayout;
+    @BindView(R.id.mine_toolbar_title)
+    TextView mineToolbarTitle;
+    @BindView(R.id.mine_toolbar_exchange)
+    ImageView mineToolbarExchange;
+    @BindView(R.id.mine_toolbar_feedback)
+    ImageView mineToolbarFeedback;
+    @BindView(R.id.mine_toolbar_setting)
+    ImageView mineToolbarSetting;
+    @BindView(R.id.user_image)
+    ImageView userImage;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,14 +48,47 @@ public class PersonFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-//        Button login =(Button) getActivity().findViewById(R.id.jump_login);
-//        login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getActivity(), "我的", Toast.LENGTH_SHORT).show();
-//                Intent intent = new Intent(getActivity(), LoginActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        mineAppbarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+                if(i >= appBarLayout.getTotalScrollRange() * 2 / 3){
+                    mineToolbarTitle.setVisibility(View.INVISIBLE);
+                }else{
+                    mineToolbarTitle.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+//    @Override
+//    public void onStart(){
+//        super.onStart();
+//        mineAppbarLayout.addOnOffsetChangedListener(this);
+//    }
+//
+//    @Override
+//    public void onResume(){
+//        super.onResume();
+//        mineAppbarLayout.addOnOffsetChangedListener(this);
+//    }
+//
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        mineAppbarLayout.removeOnOffsetChangedListener(this);
+//    }
+
+    @Override
+    public void onClick(View view){
+
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+        if(i >= appBarLayout.getTotalScrollRange() * 2 / 3){
+            mineToolbarTitle.setVisibility(View.INVISIBLE);
+        }else{
+            mineToolbarTitle.setVisibility(View.VISIBLE);
+        }
     }
 }
